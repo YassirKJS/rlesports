3f22dcf01a0e38f6df499327ede6119bcb1ea2f8
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-07-19 12:46:06 +0200

    Ordering done

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -20,29 +20,99 @@ class CardList extends Component {
             return car.name.toLowerCase().includes(this.props.searchText.toLowerCase())
         });
 
-        //ORDER
-        if (this.props.selectedOrder === 'Ascending') {
-            filteredCarsByName.sort(function(a, b){
-                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
-                if (nameA < nameB) //sort string ascending
-                    return -1;
-                if (nameA > nameB)
-                    return 1;
-                return 0; //default return value (no sorting)
-            });
-        }
-        else {
-            filteredCarsByName.sort(function(a, b){
-                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
-                if (nameA < nameB) //sort string ascending
-                    return 1;
-                if (nameA > nameB)
-                    return -1;
-                return 0; //default return value (no sorting)
-            });
-        }
 
-
+        const selectedSort = this.props.selectedSort;
+        if (selectedSort === 'name') {
+            //SORT BY NAME
+            if (this.props.selectedOrder === 'Ascending') {
+                filteredCarsByName.sort(function (a, b) {
+                    const nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+                    if (nameA < nameB) //sort string ascending
+                        return -1;
+                    if (nameA > nameB)
+                        return 1;
+                    return 0; //default return value (no sorting)
+                });
+            } else {
+                filteredCarsByName.sort(function (a, b) {
+                    const nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+                    if (nameA < nameB) //sort string descending
+                        return 1;
+                    if (nameA > nameB)
+                        return -1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+        }
+        else if (selectedSort === 'hitbox') {
+            //SORT BY HITBOX
+            if (this.props.selectedOrder === 'Ascending') {
+                filteredCarsByName.sort(function(a, b){
+                    const hitboxA = a.hitbox.toLowerCase(), hitboxB = b.hitbox.toLowerCase();
+                    if (hitboxA < hitboxB) //sort string ascending
+                        return -1;
+                    if (hitboxA > hitboxB)
+                        return 1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+            else {
+                filteredCarsByName.sort(function(a, b){
+                    const hitboxA = a.hitbox.toLowerCase(), hitboxB = b.hitbox.toLowerCase();
+                    if (hitboxA < hitboxB) //sort string descending
+                        return 1;
+                    if (hitboxA > hitboxB)
+                        return -1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+        }
+        else if (selectedSort === 'attribute') {
+            //SORT BY ATTRIBUTE
+            if (this.props.selectedOrder === 'Ascending') {
+                filteredCarsByName.sort(function(a, b){
+                    const attributeA = a.attribute.toLowerCase(), attributeB = b.attribute.toLowerCase();
+                    if (attributeA < attributeB) //sort string ascending
+                        return -1;
+                    if (attributeA > attributeB)
+                        return 1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+            else {
+                filteredCarsByName.sort(function(a, b){
+                    const attributeA = a.attribute.toLowerCase(), attributeB = b.attribute.toLowerCase();
+                    if (attributeA < attributeB) //sort string descending
+                        return 1;
+                    if (attributeA > attributeB)
+                        return -1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+        }
+        else if (selectedSort === 'release') {
+            //SORT BY RELEASE
+            if (this.props.selectedOrder === 'Ascending') {
+                filteredCarsByName.sort(function(a, b){
+                    const releaseA = a.released, releaseB = b.released;
+                    if (releaseA < releaseB) //sort string ascending
+                        return -1;
+                    if (releaseA > releaseB)
+                        return 1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+            else {
+                filteredCarsByName.sort(function(a, b){
+                    const releaseA = a.released, releaseB = b.released;
+                    if (releaseA < releaseB) //sort string descending
+                        return 1;
+                    if (releaseA > releaseB)
+                        return -1;
+                    return 0; //default return value (no sorting)
+                });
+            }
+        }
 
         return (
             <div className="cardlist">
