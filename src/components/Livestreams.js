d7465d44b6495feb0861e32c0b0dcf2d7f5fca3f
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-08-07 18:27:11 +0200

    got 1 channel data from twitch api

--- src/components/Livestreams.js
+++ src/components/Livestreams.js
@@ -6,15 +6,54 @@ import ReactPlayer from 'react-player';
 class Livestreams extends Component {
     constructor(props) {
         super(props);
+
+        console.log('This happens 1st: Constructor');
+
         this.state = {
+            loading: 'initial',
             data: []
         };
     }
 
+    loadData() {
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('This happens 6th (after 2 seconds): Data loaded');
+                //resolve('This is my data.');
+                fetch('https://api.twitch.tv/helix/streams?user_login=monstercat', {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
+                    .then(response => response.json())
+                    .then(data => {resolve(data.data) ; console.log(data.data)});
+            }, 0);
+        });
+
+        console.log('This happens 4th: Loading data');
+
+        return promise;
+    }
+
+
     componentDidMount() {
-        fetch('https://api.twitch.tv/helix/streams?user_login=metsanauris', {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
+        /*
+
+        fetch('https://api.twitch.tv/helix/streams?user_login=monstercat, {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
             .then(response => response.json())
             .then(data => {this.setState({ data: data })});
+
+        console.log('componentDidMount');
+
+        */
+
+        console.log('This happens 3rd: ComponentDidMount');
+
+        this.setState({ loading: 'true' });
+        this.loadData()
+            .then((data) => {
+                console.log('This happens 7th: ComponentDidMount/this.loadData');
+                this.setState({
+                    data: data,
+                    loading: 'false'
+                });
+            });
     }
 
     /*async fetchLiveUsers() {
@@ -22,6 +61,29 @@ class Livestreams extends Component {
     }*/
 
     render() {
+        if (this.state.loading === 'initial') {
+            console.log('This happens 2nd - after the class is constructed. You will not see this element because React is still computing changes to the DOM.');
+            return <h2>Intializing...</h2>;
+        }
+
+
+        if (this.state.loading === 'true') {
+            console.log('This happens 5th - when waiting for data.');
+            return <h2>Loading...</h2>;
+        }
+
+        console.log('This happens 8th - after I get data.');
+        return (
+            <div>
+                <p>Got some data!</p>
+                <p>{this.state.data[0].user_name}</p>
+            </div>
+        );
+
+
+
+/*
+        console.log('rendering');
         console.log(this.state.data);
 
         channels.forEach(function(channel) {
@@ -53,6 +115,7 @@ class Livestreams extends Component {
             </div>
 
         );
+*/
     }
 }
 
