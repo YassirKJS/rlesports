32e6f0d3274bdc4447447043f42bec6ebe50ee09
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-08-08 18:30:55 +0200

    wrote twitch request to get every stream

--- src/components/Livestreams.js
+++ src/components/Livestreams.js
@@ -20,7 +20,56 @@ class Livestreams extends Component {
             setTimeout(() => {
                 console.log('This happens 6th (after 2 seconds): Data loaded');
                 //resolve('This is my data.');
-                fetch('https://api.twitch.tv/helix/streams?user_login=monstercat', {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
+                fetch('https://api.twitch.tv/helix/streams' +
+                    '?user_login=' + channels[0].twitch +
+                    '&user_login='+ channels[1].twitch +
+                    '&user_login=greazymeister' + channels[2].twitch +
+                    '&user_login=mognus1' + channels[3].twitch +
+                    '&user_login=snaski' + channels[4].twitch +
+                    '&user_login=kuxir97' + channels[5].twitch +
+                    '&user_login=speed_rl' + channels[6].twitch +
+                    '&user_login=al0t97' + channels[7].twitch +
+                    '&user_login=' + channels[8].twitch +
+                    '&user_login=' + channels[9].twitch +
+                    '&user_login=' + channels[10].twitch +
+                    '&user_login=' + channels[11].twitch +
+                    '&user_login=' + channels[12].twitch +
+                    '&user_login=' + channels[13].twitch +
+                    '&user_login=' + channels[14].twitch +
+                    '&user_login=' + channels[15].twitch +
+                    '&user_login=' + channels[16].twitch +
+                    '&user_login=' + channels[17].twitch +
+                    '&user_login=' + channels[18].twitch +
+                    '&user_login=' + channels[19].twitch +
+                    '&user_login=' + channels[20].twitch +
+                    '&user_login=' + channels[21].twitch +
+                    '&user_login=' + channels[22].twitch +
+                    '&user_login=' + channels[23].twitch +
+                    '&user_login=' + channels[24].twitch +
+                    '&user_login=' + channels[25].twitch +
+                    '&user_login=' + channels[26].twitch +
+                    '&user_login=' + channels[27].twitch +
+                    '&user_login=' + channels[28].twitch +
+                    '&user_login=' + channels[29].twitch +
+                    '&user_login=' + channels[30].twitch +
+                    '&user_login=' + channels[31].twitch +
+                    '&user_login=' + channels[32].twitch +
+                    '&user_login=' + channels[33].twitch +
+                    '&user_login=' + channels[34].twitch +
+                    '&user_login=' + channels[35].twitch +
+                    '&user_login=' + channels[36].twitch +
+                    '&user_login=' + channels[37].twitch +
+                    '&user_login=' + channels[38].twitch +
+                    '&user_login=' + channels[39].twitch +
+                    '&user_login=' + channels[40].twitch +
+                    '&user_login=' + channels[41].twitch +
+                    '&user_login=' + channels[42].twitch +
+                    '&user_login=' + channels[43].twitch +
+                    '&user_login=' + channels[44].twitch +
+                    '&user_login=' + channels[45].twitch +
+                    '&user_login=' + channels[46].twitch +
+                    '&user_login=' + channels[47].twitch,
+                    {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
                     .then(response => response.json())
                     .then(data => {resolve(data.data) ; console.log(data.data)});
             }, 500);
@@ -70,8 +119,8 @@ class Livestreams extends Component {
         return (
             <div>
                 <p>Got some data!</p>
-                <p>{this.state.data[0].user_name}</p>
-                <p>{this.state.data[0].title}</p>
+                {/*<p>{this.state.data[0].user_name}</p>
+                <p>{this.state.data[0].title}</p>*/}
 
                 <div className='livestreams'>
                     {channels.map((channel) => {
