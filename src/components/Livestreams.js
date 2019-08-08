0d6c42415e6d95240333b2b29aec78608600e393
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-08-08 17:38:47 +0200

    added players images

--- src/components/Livestreams.js
+++ src/components/Livestreams.js
@@ -23,7 +23,7 @@ class Livestreams extends Component {
                 fetch('https://api.twitch.tv/helix/streams?user_login=monstercat', {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
                     .then(response => response.json())
                     .then(data => {resolve(data.data) ; console.log(data.data)});
-            }, 0);
+            }, 500);
         });
 
         console.log('This happens 4th: Loading data');
@@ -34,13 +34,11 @@ class Livestreams extends Component {
 
     componentDidMount() {
         /*
-
         fetch('https://api.twitch.tv/helix/streams?user_login=monstercat, {headers: {'Client-ID':'u6fhve63ljgmw1qz1dyck62x7tcpn2'}} )
             .then(response => response.json())
             .then(data => {this.setState({ data: data })});
 
         console.log('componentDidMount');
-
         */
 
         console.log('This happens 3rd: ComponentDidMount');
@@ -56,16 +54,12 @@ class Livestreams extends Component {
             });
     }
 
-    /*async fetchLiveUsers() {
-        const response = await fetch()
-    }*/
 
     render() {
         if (this.state.loading === 'initial') {
             console.log('This happens 2nd - after the class is constructed. You will not see this element because React is still computing changes to the DOM.');
             return <h2>Intializing...</h2>;
         }
-
 
         if (this.state.loading === 'true') {
             console.log('This happens 5th - when waiting for data.');
@@ -77,45 +71,28 @@ class Livestreams extends Component {
             <div>
                 <p>Got some data!</p>
                 <p>{this.state.data[0].user_name}</p>
+                <p>{this.state.data[0].title}</p>
+
+                <div className='livestreams'>
+                    {channels.map((channel) => {
+                        return (
+                            <div className='livestream-card'>
+                                <ReactPlayer
+                                    key={channel.id}
+                                    url={'https://www.twitch.tv/' + channel.twitch}
+                                    width="32rem"
+                                    height="20rem"
+                                    controls={true}
+                                    playing
+                                    playsinline
+                                    light={"https://static-cdn.jtvnw.net/previews-ttv/live_user_" + channel.twitch + "-640x360.jpg"}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         );
-
-
-
-/*
-        console.log('rendering');
-        console.log(this.state.data);
-
-        channels.forEach(function(channel) {
-
-        });
-
-        return (
-            <div className='livestreams'>
-                {channels.map((channel) => {
-                    return ( <ReactPlayer
-                        key={channel.id}
-                        url={'https://www.twitch.tv/' + channel.twitch}
-                        width="32rem"
-                        height="20rem"
-                        controls={true}
-                        playing
-                        playsinline
-                        light={"https://static-cdn.jtvnw.net/previews-ttv/live_user_" + channel.twitch + "-640x360.jpg"}
-
-                        //onReady={console.log(channel.id + ': ' + channel.twitch + ' onReady')}
-                        //onPlay={console.log(channel.id + ': ' + channel.twitch + ' onPlay')}
-                        //onPause={console.log(channel.id + ': ' + channel.twitch + ' onPause')}
-                        //onBuffer={console.log(channel.id + ': ' + channel.twitch + ' onBuffer')}
-                        //onBufferEnd={console.log(channel.id + ': ' + channel.twitch + ' onBufferEnd')}
-                        //onEnded={console.log(channel.id + ': ' + channel.twitch + ' onEnded')}
-                        //onError={console.log(channel.id + ': ' + channel.twitch + ' onError')}
-                    />);
-                })}
-            </div>
-
-        );
-*/
     }
 }
 
