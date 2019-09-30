4ec292f4e600a10c0cf40470dcedfada8b11d43d
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-09-30 03:02:44 +0100

    re: major commit: livestreams redesign code cleaning

--- src/components/livestreams/LivestreamCard.js
+++ src/components/livestreams/LivestreamCard.js
@@ -7,129 +7,118 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 import ReactPlayer from "react-player";
 
-
 class LivestreamCard extends Component {
 
-    
-    render() {
-        const channel = this.props.channel;
-        const data = this.props.data;
-        const id = channel.id;
-        const ign = channel.ign;
-        const twitch = channel.twitch;
-        const twitter = channel.twitter;
-        const avatar = require('../../resources/imgs/players/' + ign + '.png');
-        
-        const banner = require('../../resources/imgs/design/name_banner.png');
-        const banner_circle = require('../../resources/imgs/design/circle.png');
-        const banner_rectangle = require('../../resources/imgs/design/rectangle.png');
-        const banner_semi_rectangle = require('../../resources/imgs/design/semi-rectangle.png');
-        const banner_rectangle_inside = require('../../resources/imgs/design/rectangle-inside.png');
-        const banner_rectangle_outside = require('../../resources/imgs/design/rectangle-outside.png');
-        const banner_rectangle_side = require('../../resources/imgs/design/rectangle-side.png');
-        const banner_rectangle_side_2 = require('../../resources/imgs/design/rectangle-side-2.png');
-        const banner_rectangle_side_21 = require('../../resources/imgs/design/rectangle-side-2-1.png');
-        const banner_rectangle_side_22 = require('../../resources/imgs/design/rectangle-side-2-2.png');
+  render() {
+      const channel = this.props.channel;
+      const data = this.props.data;
+      const id = channel.id;
+      const ign = channel.ign;
+      const twitch = channel.twitch;
+      const twitter = channel.twitter;
+      const avatar = require('../../resources/imgs/players/' + ign + '.png');
+      
+      const banner_circle = require('../../resources/imgs/design/circle.png');
+      const banner_semi_rectangle = require('../../resources/imgs/design/semi-rectangle.png');
+      const banner_rectangle_inside = require('../../resources/imgs/design/rectangle-inside.png');
+      const banner_rectangle_side = require('../../resources/imgs/design/rectangle-side.png');
+      const banner_rectangle_side_21 = require('../../resources/imgs/design/rectangle-side-2-1.png');
+      const banner_rectangle_side_22 = require('../../resources/imgs/design/rectangle-side-2-2.png');
 
-        let viewerCount = this.props.viewerCount;
+      let viewerCount = this.props.viewerCount;
 
-        const liquipediaIcon = require('../../resources/imgs/liquipedia.png');
+      const liquipediaIcon = require('../../resources/imgs/liquipedia.png');
 
+      let title = "";
+      data.forEach(function(data_) {
+          if (data_.user_name.toLowerCase() === twitch.toLowerCase()) {
+              title = data_.title;
+          }
+      });
+      
+      return (
+        <div className='livestream-card' >
+          <ReactPlayer
+              className="livestream-player"
+              key={id}
+              url={'https://www.twitch.tv/' + twitch}
+              width="100%"
+              height="23rem"
+              controls={true}
+              playing
+              playsinline
+              light={"https://static-cdn.jtvnw.net/previews-ttv/live_user_" + twitch + "-640x360.jpg"}
+          />      
 
-        let title = "";
-        data.forEach(function(data_) {
-            if (data_.user_name.toLowerCase() === twitch.toLowerCase()) {
-                title = data_.title;
-            }
-        });
-        
+          {
+            viewerCount === undefined ? 
+              <div className='livestream__viewerCount'>Offline</div> 
+              : 
+              <div className='livestream__viewerCount'>
+                <FontAwesomeIcon icon={faUser} className='livestream__viewerCount--icon'/>
+                {viewerCount}
+              </div>
+          }
+                        
+          <div className="livestream__navigation">
+              <input type="checkbox" className="livestream__navigation--checkbox" id={"navi-toggle-" + id}></input>
+              <label htmlFor={"navi-toggle-" + id} className="livestream__navigation--button">
+                  <span className="livestream__navigation--icon">&nbsp;</span>
+              </label>
 
-        return (
-          <div className='livestream-card' >
-            <ReactPlayer
-                className="livestream-player"
-                key={id}
-                url={'https://www.twitch.tv/' + twitch}
-                width="100%"
-                height="23rem"
-                controls={true}
-                playing
-                playsinline
-                light={"https://static-cdn.jtvnw.net/previews-ttv/live_user_" + twitch + "-640x360.jpg"}
-            />      
+              <div className="livestream__navigation--background">
+                  <a className="livestream__navigation--item--1" href={'https://www.instagram.com/' + twitch} target="_blank" > <img alt="liquipedia" src={liquipediaIcon} /></a>
+                  <a className="livestream__navigation--item--2" href={'https://www.twitch.tv/' + twitch} target="_blank" ><FontAwesomeIcon icon={faTwitch} /></a>
+                  <a className="livestream__navigation--item--3" href={'https://youtube.com/' + twitter} target="_blank" ><FontAwesomeIcon icon={faYoutube} /></a>
+                  <a className="livestream__navigation--item--4" href={'https://twitter.com/' + twitter} target="_blank" ><FontAwesomeIcon icon={faTwitter} /></a>
+              </div>
+          </div>
 
-            {
-              viewerCount === undefined ? 
-                <div className='livestream__viewerCount'>Offline</div> 
-                : 
-                <div className='livestream__viewerCount'>
-                  <FontAwesomeIcon icon={faUser} className='livestream__viewerCount--icon'/>
-                  {viewerCount}
-                </div>
-            }
-                          
-            <div className="livestream__navigation">
-                <input type="checkbox" className="livestream__navigation--checkbox" id={"navi-toggle-" + id}></input>
-                <label htmlFor={"navi-toggle-" + id} className="livestream__navigation--button">
-                    <span className="livestream__navigation--icon">&nbsp;</span>
-                </label>
-
-                <div className="livestream__navigation--background">
-                    <a className="livestream__navigation--item--1" href={'https://www.instagram.com/' + twitch} target="_blank" > <img alt="liquipedia" src={liquipediaIcon} /></a>
-                    <a className="livestream__navigation--item--2" href={'https://www.twitch.tv/' + twitch} target="_blank" ><FontAwesomeIcon icon={faTwitch} /></a>
-                    <a className="livestream__navigation--item--3" href={'https://youtube.com/' + twitter} target="_blank" ><FontAwesomeIcon icon={faYoutube} /></a>
-                    <a className="livestream__navigation--item--4" href={'https://twitter.com/' + twitter} target="_blank" ><FontAwesomeIcon icon={faTwitter} /></a>
-                </div>
-            </div>
-
-            <div className="channel">
-                {/*<a className="channel__img-link" href={'https://www.twitch.tv/' + twitch} target="_blank">
-                    <img alt="avatar" src={avatar} height="250" width="250" className="channel__img"/>
-                </a>*/}
-                {
-                  (title === '') ?
-       
-                      <div className='channel__banner'>                                                 
-                        <div className='channel__banner--circle'>
-                          <img alt='' src={banner_circle} className='channel__banner--circle--bg'/>
-                          <a className="channel__banner--img-link" href={'https://www.twitch.tv/' + twitch} target="_blank">
-                            <img alt="avatar" src={avatar} height="250" width="250" className="channel__banner--avatar"/>
-                          </a> 
-                        </div>
-                        <div className='channel__banner--rectangle'>
-                          <img alt='' src={banner_semi_rectangle} className='channel__banner--rectangle--bg'/>
-                          <span className='channel__banner--name'>{ign}</span>
-                        </div>                                                                      
-                      </div>
-                    
-                    :
-
-                    <div className='channel__banner__live'>                                                 
-                      <div className='channel__banner__live--circle'>
-                        <img alt='' src={banner_circle} className='channel__banner__live--circle--bg'/>
+          <div className="channel">
+              {
+                (title === '') ?
+      
+                    <div className='channel__banner'>                                                 
+                      <div className='channel__banner--circle'>
+                        <img alt='' src={banner_circle} className='channel__banner--circle--bg'/>
                         <a className="channel__banner--img-link" href={'https://www.twitch.tv/' + twitch} target="_blank">
-                          <img alt="avatar" src={avatar} height="250" width="250" className="channel__banner__live--avatar"/>
+                          <img alt="avatar" src={avatar} height="250" width="250" className="channel__banner--avatar"/>
                         </a> 
                       </div>
-                      <div className='channel__banner__live--rectangle'>
-                        <div className='channel__banner__live--rectangle--inside'>
-                          <img alt='' src={banner_rectangle_inside} className='channel__banner__live--rectangle--inside--bg'/>
-                          <span className='channel__banner__live--title'>{title}</span>
-                          <span className='channel__banner__live--name'>{ign}</span> 
-                        </div>
-                        <div className='channel__banner__live--rectangle--outside'>
-                          <img alt='' src={banner_rectangle_side} className='channel__banner__live--rectangle--outside--bg--1'/>
-                          
-                          <img alt='' src={banner_rectangle_side_21} className='channel__banner__live--rectangle--outside--bg--21'/>
-                          <img alt='' src={banner_rectangle_side_22} className='channel__banner__live--rectangle--outside--bg--22'/>
-                        </div>                                                                       
+                      <div className='channel__banner--rectangle'>
+                        <img alt='' src={banner_semi_rectangle} className='channel__banner--rectangle--bg'/>
+                        <span className='channel__banner--name'>{ign}</span>
                       </div>                                                                      
                     </div>
-                }
-            </div>
+                  
+                  :
+
+                  <div className='channel__banner__live'>                                                 
+                    <div className='channel__banner__live--circle'>
+                      <img alt='' src={banner_circle} className='channel__banner__live--circle--bg'/>
+                      <a className="channel__banner--img-link" href={'https://www.twitch.tv/' + twitch} target="_blank">
+                        <img alt="avatar" src={avatar} height="250" width="250" className="channel__banner__live--avatar"/>
+                      </a> 
+                    </div>
+                    <div className='channel__banner__live--rectangle'>
+                      <div className='channel__banner__live--rectangle--inside'>
+                        <img alt='' src={banner_rectangle_inside} className='channel__banner__live--rectangle--inside--bg'/>
+                        <span className='channel__banner__live--title'>{title}</span>
+                        <span className='channel__banner__live--name'>{ign}</span> 
+                      </div>
+                      <div className='channel__banner__live--rectangle--outside'>
+                        <img alt='' src={banner_rectangle_side} className='channel__banner__live--rectangle--outside--bg--1'/>
+                        
+                        <img alt='' src={banner_rectangle_side_21} className='channel__banner__live--rectangle--outside--bg--21'/>
+                        <img alt='' src={banner_rectangle_side_22} className='channel__banner__live--rectangle--outside--bg--22'/>
+                      </div>                                                                       
+                    </div>                                                                      
+                  </div>
+              }
           </div>
-        );
-    }
+        </div>
+      );
+  }
 }
 
 export default LivestreamCard;
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,10 @@
-$('#maindiv').width($('#div1').width());
+function mouseoverBox1(){
+  $(".channel__banner__live--rectangle--outside--bg--21").bind("animationEnd", function(){
+    $(this).removeClass("hovered")  
+  })
+  
+  $(".channel__banner__live--rectangle--outside--bg--21").hover(function(){
+    $(this).addClass("hovered");        
+  })
+}
+
