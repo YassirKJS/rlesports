67789abec4c9a913f70db97a2aeeef81e0a2bd4e
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-10-06 23:49:38 +0100

    sigin form

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -42,37 +42,43 @@ class Signin extends React.Component {
     return (
       <Layout>
         <div className='signin'>
-          <div className='signin__header'>Sign In</div>
+          <div className='signin__header'>SIGN IN</div>
 
-          <div className='signin__form'>            
+          <div className='signin__form form__group'>            
             <div className='signin__email'>
-              <label className='signin__email--label'>Email</label><br/>
               <input 
-                className='signin__email--input'
+                className='signin__email--input form__input'
                 type='email'
                 name="email-address"
                 id="email-address"
+                placeholder="Email Address"
+                required
               />
+              <label for='email-address' className='signin__email--label form__label'>Email Address</label>
             </div>
 
-            <div className='signin__password'>
-              <label className='signin__password--label'>Password</label><br/>
+            <div className='signin__password form__group'>              
               <input 
-                className='signin__password--input'
+                className='signin__password--input form__input'
                 type='password'
                 name="password"
                 id="password"
+                placeholder="Password"
+                required
               />
+              <label for='password' className='signin__password--label form__label'>Password</label>
             </div>
 
-            <div className='signin__remember'>
-              <label>
+            <div className='signin__remember form__checkbox-group'>
                 <input 
-                  className='signin__remember--input'
+                  className='signin__remember--input form__checkbox-input'
                   type='checkbox'
+                  id="remember"
                 />
-                <span className='signin__remember--span'> Remember me </span>
-              </label>
+                <label for="remember" class="form__checkbox-label">
+                    <span className="form__checkbox-button"></span>
+                    Remember me
+                </label>
             </div>
 
             <div className='signin__submit'>
@@ -85,8 +91,8 @@ class Signin extends React.Component {
           </div>
 
           <div className='signin__footer'>
-            <div className='signin__footer--signup'>Sign up</div>
-            <div className='signin__footer--forgot'>Forgot your password?</div>
+            <div className='signin__footer--signup'>Sign up !</div>
+            <div className='signin__footer--forgot'>Forgot your password ?</div>
           </div>
 
         </div>
@@ -96,3 +102,5 @@ class Signin extends React.Component {
 }
 
 export default Signin;
+
+//&rarr;
